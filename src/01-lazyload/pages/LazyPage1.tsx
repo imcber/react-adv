60578546518c5508280142513572d81a777c0c50
Ekatl <bernardo.ramirez@tcpip.tech>
2021-12-06 21:41:05 -0600

    basic lazyload

--- src/01-lazyload/pages/LazyPage1.tsx
+++ src/01-lazyload/pages/LazyPage1.tsx
@@ -1,3 +1,5 @@
 export const LazyPage1 = () => {
 	return <h1>Lazy pages 1</h1>;
 };
+
+export default LazyPage1;
--- src/01-lazyload/pages/LazyPage2.tsx
+++ src/01-lazyload/pages/LazyPage2.tsx
@@ -1,3 +1,5 @@
 export const LazyPage2 = () => {
 	return <h1>Lazy pages 2</h1>;
 };
+
+export default LazyPage2;
